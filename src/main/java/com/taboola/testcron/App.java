f24ba04fc961aeb0ac8aba3a117de128d66186ae
Victor Trakhtenberg <victor.t@taboola.com>
2017-05-16 10:46:26 +0300

    first commit

---
+++ src/main/java/com/taboola/testcron/App.java
@@ -0,0 +1,13 @@
+package com.taboola.testcron;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
